15329c5d46b617ee503da730182b65a5f55410a5
Jagdish Singh <jagdish.chauhan01@gmail.com>
2017-09-19 23:15:38 +0530

    Reduce collisions

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,9 @@
 height= 270;
 width= 480;
 var i=0;
+var l;
 var new_move;
+var last_move;
 var last_score;
 var last_moves=[];
 var speed=[-.1,-.2,-.3,-.4,-.5,-.6,-.7,-.8,-.9];
@@ -27,18 +29,40 @@ var myGameArea = {
         mySound.play();
     },
     reset : function() {
-        last_score=score;
         myObstacles= [];
         new_move = speed[Math.floor(Math.random()*9)];
         if(score==last_score){
-            new_move=acc.splice([Math.floor(Math.random()*acc.length)],1);
+            if(crash_pos=="top" && last_move){
+                last_move += .1;
+                i=acc.indexOf(last_move);
+                if(i!=undefined || i!=null){
+                    while(i<acc.length){
+                        acc.pop();
+                    }
+                }
+            }
+            else if(crash_pos=="bottom" && last_move){
+                last_move -= .1;
+                i=acc.indexOf(last_move);
+                if(i!=undefined || i!=null){
+                    l=0;
+                    while(l<i){
+                        l++;
+                        acc.shift();
+                    }
+                }
+            }else{}
+
+            crash_pos=null;
             if(acc.length==0){
                 score-=1;
                 acc=speed.slice();
             }
+            new_move=acc.splice([Math.floor(Math.random()*acc.length)],1);
             //console.log(acc);
             //console.log(new_move);
         }else{
+            last_score=score;
             acc=speed.slice();
         }
         //console.log(new_move);
@@ -53,6 +77,7 @@ var myGameArea = {
                 accelerations[i]=speed[Math.floor(Math.random()*9)];
             }
         }
+        last_move=new_move;
         console.log(score+1);
         k=0;
         j=0;
